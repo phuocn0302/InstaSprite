af6976acc9aeec1a5c4ca09e972e583ade5274e7
phuocn0302 <phuocnguyen230302@gmail.com>
2025-06-01 15:23:41 +0700

    refactor (LoadISpriteDialog): seperate CanvasPreviewer for reusable

--- app/src/main/java/com/olaz/instasprite/ui/screens/drawingscreen/dialog/LoadISpriteDialog.kt
+++ app/src/main/java/com/olaz/instasprite/ui/screens/drawingscreen/dialog/LoadISpriteDialog.kt
@@ -4,18 +4,14 @@ import android.net.Uri
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Icon
@@ -27,11 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,11 +32,10 @@ import com.olaz.instasprite.data.model.ISpriteData
 import com.olaz.instasprite.data.model.PixelCanvasModel
 import com.olaz.instasprite.data.repository.PixelCanvasRepository
 import com.olaz.instasprite.data.repository.StorageLocationRepository
+import com.olaz.instasprite.ui.components.composable.CanvasPreviewer
 import com.olaz.instasprite.ui.components.dialog.CustomDialog
 import com.olaz.instasprite.ui.screens.drawingscreen.DrawingScreenViewModel
-import com.olaz.instasprite.ui.theme.DrawingScreenColor
 import com.olaz.instasprite.utils.getFileName
-import kotlin.math.min
 
 @Composable
 fun LoadISpriteDialog(
@@ -120,47 +111,11 @@ fun LoadISpriteDialog(
 
             spriteData?.let {
                 Spacer(Modifier.height(12.dp))
-                PreviewCanvas(spriteData!!)
+                CanvasPreviewer(spriteData!!)
             }
         }
     })
 }
-
-@Composable
-fun PreviewCanvas(spriteData: ISpriteData) {
-    val spriteAspectRatio = spriteData.width.toFloat() / spriteData.height.toFloat()
-
-    Canvas(
-        modifier = Modifier
-            .fillMaxWidth()
-            .heightIn(max = 300.dp)
-            .aspectRatio(spriteAspectRatio, matchHeightConstraintsFirst = true)
-            .border(5.dp, DrawingScreenColor.PaletteBackgroundColor)
-            .clipToBounds()
-    ) {
-        val pixelSize = min(
-            size.width / spriteData.width,
-            size.height / spriteData.height
-        )
-
-        spriteData.pixelsData.forEachIndexed { index, colorInt ->
-            val x = index % spriteData.width
-            val y = index / spriteData.width
-
-            val color = Color(colorInt)
-
-            drawRect(
-                color = color,
-                topLeft = Offset(
-                    x = x * pixelSize,
-                    y = y * pixelSize
-                ),
-                size = Size(pixelSize, pixelSize)
-            )
-        }
-    }
-}
-
 
 @Composable
 @Preview
